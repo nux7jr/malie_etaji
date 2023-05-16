edf69ab5201d53a52982a73006be5d9343159d5a
Denis Zarubin <Denis13216@gmail.com>
2023-05-16 11:48:59 +0700

    add custom 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    public function render($request, Exception|Throwable $exception)
+    {
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return response()->view('errors.' . '404', [], 404);
+            }
+        }
+        return parent::render($request, $exception);
+    }
 }
