a314cfed794aca9582d07a48355961d560ae1120
Denis Zarubin <Denis13216@gmail.com>
2023-06-20 12:03:41 +0700

    new build

--- public/build/assets/feedback-9e57aa4b.js
+++
@@ -1,1 +0,0 @@
-import"./index-706639a7.js";function t(e){new e(".feedback-swiper",{slidesPerView:1,spaceBetween:20,navigation:{nextEl:".feedback-button-next",prevEl:".feedback-button-prev"},breakpoints:{999:{slidesPerView:3,spaceBetweenSlides:60}}})}export{t as i};
