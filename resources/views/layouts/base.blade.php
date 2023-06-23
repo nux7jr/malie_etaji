6c2e6286d6a2a5c80ac4a63b65856011e8cacef7
nux7jr <nux7jr@gmail.com>
2023-06-23 11:03:37 +0700

    add favicno

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -27,11 +27,11 @@
     @vite('resources/css/components/mob_menu.css')
 
     {{-- favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::image('favicon/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::image('favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::image('favicon/favicon-16x16.png') }}">
-    {{--
-    <link rel="manifest" href="/site.webmanifest"> --}}
+    <link rel="apple-touch-icon" sizes="180x180" href=" {{ Vite::image('favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::image('favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::image('favicon/favicon-16x16.png')}}">
+
+    <link rel="mask-icon" href="{{ Vite::image('favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
