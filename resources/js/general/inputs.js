d1924c2e700a993a968cf8fb130c817b5237e34e
Denis Zarubin <Denis13216@gmail.com>
2023-06-27 11:30:26 +0700

    worked calc

--- resources/js/general/inputs.js
+++ resources/js/general/inputs.js
@@ -124,26 +124,32 @@ if (calc !== null){
 calcDropdownEvents(calc);
 function calcDropdownEvents(calc){
     if (calc !== null){
+        document.querySelectorAll('.mortgage-calculator .dropdown-select-items div').forEach((itm) => {
+            try {
+                itm.removeEventListener('click', addListenerSelectCalc,true);
+            }catch (err){}
+        });
         calc.querySelectorAll('.dropdown-select-items div').forEach((evt) => {
-            evt.addEventListener('click', function (e) {
-                const selected = document.querySelector('.dropdown-select-selected');
-                selected.removeAttribute('class');
-                const tempThis = this.innerHTML;
-                const tempPercent = this.getAttribute('percent')
-                selected.removeAttribute('class');
-                this.setAttribute('percent',selected.getAttribute('percent'));
-
-                selected.setAttribute('percent',tempPercent);
-                this.innerHTML = selected.innerHTML;
-                selected.innerHTML = tempThis;
-                selected.classList.add('dropdown-select-selected');
-                const newSelected = document.querySelector('.dropdown-select-selected');
-                selectClickEvent(newSelected);
-                renderCalc();
-                calcDropdownEvents(document.querySelector('.mortgage-calculator'));
-            });
+            evt.addEventListener('click', addListenerSelectCalc);
         });
     }
+}
+function addListenerSelectCalc (e) {
+    const selected = document.querySelector('.dropdown-select-selected');
+    selected.removeAttribute('class');
+    const tempThis = this.innerHTML;
+    const tempPercent = this.getAttribute('percent')
+    selected.removeAttribute('class');
+    this.setAttribute('percent',selected.getAttribute('percent'));
+
+    selected.setAttribute('percent',tempPercent);
+    this.innerHTML = selected.innerHTML;
+    selected.innerHTML = tempThis;
+    selected.classList.add('dropdown-select-selected');
+    const newSelected = document.querySelector('.dropdown-select-selected');
+    selectClickEvent(newSelected);
+    renderCalc();
+    calcDropdownEvents(document.querySelector('.mortgage-calculator'));
 }
 function selectClickEvent(select){
     select.addEventListener("click", function(ev) {
