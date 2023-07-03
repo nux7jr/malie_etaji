f2c3b9d970cbe37094cf99cb74e087dd2104e618
nux7jr <nux7jr@gmail.com>
2023-07-03 14:53:42 +0700

    add new pixel

--- resources/views/landings/quiz/index.blade.php
+++ resources/views/landings/quiz/index.blade.php
@@ -115,36 +115,20 @@ session_start();
 
     <!-- Top.Mail.Ru counter -->
     <script type="text/javascript">
-        var _tmr = window._tmr || (window._tmr = []);
-        _tmr.push({
-            id: "3323696",
-            type: "pageView",
-            start: (new Date()).getTime()
-        });
-        (function(d, w, id) {
-            if (d.getElementById(id)) return;
-            var ts = d.createElement("script");
-            ts.type = "text/javascript";
-            ts.async = true;
-            ts.id = id;
-            ts.src = "https://top-fwz1.mail.ru/js/code.js";
-            var f = function() {
-                var s = d.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(ts, s);
-            };
-            if (w.opera == "[object Opera]") {
-                d.addEventListener("DOMContentLoaded", f, false);
-            } else {
-                f();
-            }
-        })(document, window, "tmr-code");
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "3366612", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+      if (d.getElementById(id)) return;
+      var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+      ts.src = "https://top-fwz1.mail.ru/js/code.js";
+      var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+      if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "tmr-code");
     </script>
-    <noscript>
-        <div><img src="https://top-fwz1.mail.ru/counter?id=3323696;js=na" style="position:absolute;left:-9999px;"
-                  alt="Top.Mail.Ru" /></div>
-    </noscript>
+    <noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3366612;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div></noscript>
     <!-- /Top.Mail.Ru counter -->
 
+    
     <script type="text/javascript">
         ! function() {
             var t = document.createElement("script");
